788c90883e15c7ab3cb3dd1f8dcd67d68489e3f4
taklittletooth <slwoods77@yahoo.co.jp>
2022-09-19 12:18:18 +0900

    first commit

---
+++ work-spring/tsubuyaki_spring/src/main/java/work/example/tsubuyaki_spring/TsubuyakiSpringApplication.java
@@ -0,0 +1,13 @@
+package work.example.tsubuyaki_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsubuyakiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsubuyakiSpringApplication.class, args);
+	}
+
+}
---
+++ work-spring/tsubuyaki_spring/src/test/java/work/example/tsubuyaki_spring/TsubuyakiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package work.example.tsubuyaki_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsubuyakiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
